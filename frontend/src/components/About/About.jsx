affadb086b397f1208e322d6794cce4caae4d683
W P S Sandaruwan <97351293+Sandaruwan01@users.noreply.github.com>
2025-05-26 16:01:02 +0530

    added about page

---
+++ frontend/src/components/About/About.jsx
@@ -0,0 +1,18 @@
+import "./About.css"
+import Header from '../Header/Header'
+import Banner from '../banner/banner'
+import Footer from '../Footer/Footer'
+
+const About = () => {
+  return (
+    <div>
+     <Header/>
+   
+     <Banner/>
+     <Footer/>
+     
+    </div>
+  )
+}
+
+export default About
